288f30a164e856fd0fc966e1b299a48f1fe2dd87
Healingwave <legendtto@gmail.com>
2018-01-21 16:26:44 +0700

    sixth commit

--- src/main/java/api/servlet/BuyProductServlet.java
+++ src/main/java/api/servlet/BuyProductServlet.java
@@ -46,7 +46,7 @@ public class BuyProductServlet extends HttpServlet {
 			String connectionPassword = "root";
 			conn = DriverManager.getConnection(connectionUrl, connectionUser, connectionPassword);
             stmt = conn.createStatement();
-			stmt.executeUpdate("INSERT INTO receipt (receipt_id, member_id, date_time, total, shop_name) VALUES ( "+receipt.getReceiptId()+", "+receipt.getMemberId()+", 2561-01-21, "+receipt.getTotal()+", '"+receipt.getShopName()+"');");
+			stmt.executeUpdate("INSERT INTO receipt (receipt_id, member_id, date_time, total, shop_name) VALUES ( "+receipt.getReceiptId()+", "+receipt.getMemberId()+", '2561-01-21', "+receipt.getTotal()+", '"+receipt.getShopName()+"');");
         }catch (Exception e) {
 			e.printStackTrace();
 		} 
